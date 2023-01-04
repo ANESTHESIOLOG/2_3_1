f203bf211cf4bd65ab9b8499c31809fe3bd341aa
zotov <zotovegor952@gmail.com>
2023-01-04 20:09:37 +0300

    Надеюсь назвал метод глаголом

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDao;
+import web.service.CarService;
 
 @Controller
 @RequestMapping("/cars")
@@ -21,18 +21,18 @@ public class CarsController {
 //при /cars?count=3 - из 3, и тд. При count ≥ 5 выводить весь список машин.
 
 
-    private final CarDao carDao;
+    private final CarService car;
 
     @Autowired
-    public CarsController(CarDao carDao) {
-        this.carDao = carDao;
+    public CarsController(CarService car) {
+        this.car = car;
     }
 
 
     @GetMapping()
     public String index(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("car", carDao.carRemoval(count));
-        return "removCarCount";
+        model.addAttribute("car", car.returningCarListCount(count));
+        return "carListCount";
     }
 
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,25 +1,29 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    private CarDao carDao;
+    private List<Car> car;
 
-    @Autowired
-    public CarServiceImpl(CarDao carDao) {
-        this.carDao = carDao;
+    {
+        car = new ArrayList<>();
+
+        car.add(new Car(1, "Lada", "Vesta"));
+        car.add(new Car(2, "Mersedece", "GLS"));
+        car.add(new Car(3, "BMW", "5-series"));
+        car.add(new Car(4, "KIA", "Rio"));
+        car.add(new Car(5, "Cherry", "Tiggo"));
     }
 
     @Override
-    public List<Car> carRemoval(int count) {
-        return carDao.carRemoval(count);
+    public List<Car> returningCarListCount(int count) {
+        return car.stream().limit(count).collect(Collectors.toList());
     }
 }
